f2ac2b0f7162b166dbd3d35d42c1759585fcf52b
Sidney Liebrand <sidneyliebrand@gmail.com>
2023-03-22 03:54:40 +0100

    Clean up and slightly reduce reads in extractors/png.js

--- src/extractors/png.js
+++ src/extractors/png.js
@@ -2,34 +2,21 @@ import {lazystream} from '../util.js';
 
 export function attributes(input) {
     const stream = lazystream(input);
-    const isMNG = stream.take(4).includes('MNG');
-    const isFried = stream.skip(8).take(4).includes('CgBI');
+    const header = stream.take(16);
 
-    if (isFried) stream.skip(16);
+    if (header.includes('CgBI')) stream.skip(16);
 
-    const result = {
-        width: stream.takeUInt32BE(),
-        height: stream.takeUInt32BE(),
-        size: stream.size(),
-        mime: isMNG
-            ? 'video/x-mng'
-            : isAPNG(stream)
-            ? 'image/apng'
-            : 'image/png',
-    };
+    const dimensions = stream.take(8);
+    const width = dimensions.readUInt32BE();
+    const height = dimensions.readUInt32BE(4);
+    const mime = header.includes('MNG')
+        ? 'video/x-mng'
+        : stream.take(128).includes('acTL')
+        ? 'image/apng'
+        : 'image/png';
+    const result = {width, height, size: stream.size(), mime};
 
     stream.close();
-
-    return result;
-}
-
-function isAPNG(stream) {
-    const position = stream.position();
-    const result = [256, 512, 1024].some((size) =>
-        stream.take(size).includes('acTL')
-    );
-
-    stream.goto(position);
 
     return result;
 }
